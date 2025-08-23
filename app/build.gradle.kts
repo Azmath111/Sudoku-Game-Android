f4c5169704db204514e6bf963b891d10cedb16ca
Deepak Kumar <deepak.azmath@gmail.com>
2025-08-24 03:18:32 +0530

    Style Sudoku UI and fix experimental API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("androidx.compose.ui:ui:$composeUiVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeUiVersion")
     implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.material:material-icons-extended:$composeUiVersion")
     implementation("com.google.android.material:material:1.11.0")
     debugImplementation("androidx.compose.ui:ui-tooling:$composeUiVersion")
     testImplementation("junit:junit:4.13.2")
